8959bf2bbebd9cea27cff3d55aa589e5495ac085
kolaPsag <97811724+koloPsag@users.noreply.github.com>
2022-06-25 09:24:43 +0200

    Delete src/main directory

--- src/main/java/com/kolapsag/top/utils/ChatUtil.java
+++
@@ -1,5 +0,0 @@
-package com.kolapsag.top.utils;
-
-import net.kyori.adventure.text.format.TextColor;
-
-public class ChatUtil { public static TextColor hex(String code) { return TextColor.fromHexString("#" + code); } }
